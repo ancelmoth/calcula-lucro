98145c445b48fd98f3a1c309a4ca6c9d817ace39
ancelmoth <ancelmoth@gmail.com>
2023-03-13 13:06:28 -0300

    atualizando

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 function calcular() {
-    // Obter os valores do custo e da margem de lucro
+    // Obter os valores do custo, margem de lucro e valor do serviço
     var custo = parseFloat(document.getElementById("custo").value);
     var margem = parseFloat(document.getElementById("margem").value);
-    var servico = parseFloat(document.getElementById("servico").value)
+    var servico = parseFloat(document.getElementById("servico").value);
 
-    // Calcular o preço de venda
+    // Calcular o preço de venda do produto
     var precoProduto = custo * (1 + (margem / 100));
 
     // Calcular o preço total (produto + serviço)
-    var precoTotal = precoProduto + servico
+    var precoTotal = precoProduto + servico;
 
-    // Arredondar o resultado para 2 casas decimais
+    // Arredondar os resultados para 2 casas decimais
     precoProduto = precoProduto.toFixed(2);
-    precoTotal = precoTotal.toFixed(2)
+    precoTotal = precoTotal.toFixed(2);
 
-    // Exibir o resultado na caixa de texto
+    // Exibir os resultados nas caixas de texto
     document.getElementById("preco").value = "R$ " + precoTotal;
 }
