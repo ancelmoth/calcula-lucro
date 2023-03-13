35c54f1521c285fb3694488d35b3cd6b200885c0
ancelmoth <ancelmoth@gmail.com>
2023-03-13 12:35:59 -0300

    foi added uma funcao que calcula valor total

--- app.js
+++ app.js
@@ -2,13 +2,18 @@ function calcular() {
     // Obter os valores do custo e da margem de lucro
     var custo = parseFloat(document.getElementById("custo").value);
     var margem = parseFloat(document.getElementById("margem").value);
+    var servico = parseFloat(document.getElementById("servico").value)
 
     // Calcular o preço de venda
-    var preco = custo * (1 + (margem / 100));
+    var precoProduto = custo * (1 + (margem / 100));
+
+    // Calcular o preço total (produto + serviço)
+    var precoTotal = precoProduto + servico
 
     // Arredondar o resultado para 2 casas decimais
-    preco = preco.toFixed(2);
+    precoProduto = precoProduto.toFixed(2);
+    precoTotal = precoTotal.toFixed(2)
 
     // Exibir o resultado na caixa de texto
-    document.getElementById("preco").value = "R$ " + preco;
+    document.getElementById("preco").value = "R$ " + precoTotal;
 }
